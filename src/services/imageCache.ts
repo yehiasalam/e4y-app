9934a687436ea36e94661f9b975a75a4d46d9ff9
Yehia A.Salam <yehiasalam@live.com>
2018-06-24 00:50:39 +0200

    - birth

---
+++ src/services/imageCache.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Platform }   from 'ionic-angular';
+
+import ImgCache       from 'imgcache.js';
+
+/**
+ * This service is charged of provide the methods to cache the images
+ */
+@Injectable()
+export class ImgcacheService {
+
+  public imgQueue: string[] = [];
+
+  constructor(platform: Platform) {
+    ImgCache.options.debug = true;
+  }
+
+  /**
+   * Init imgCache library
+   * @return {Promise}
+   */
+  public initImgCache(): Promise<any> {
+    console.log('Image cache init');
+    return new Promise((resolve, reject) => {
+      if (ImgCache.ready) {
+        console.log('Image cache ready');
+        resolve();
+      } else {
+        ImgCache.init(() => resolve(), () => reject());
+      }
+    });
+  }
+
+  /**
+   * Cache images
+   * @param src {string} - img source
+   */
+  public cacheImg(src: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if(ImgCache.ready){
+        ImgCache.isCached(src, (path: string, success: boolean) => {
+          // if not, it will be cached
+          if (success) {
+            ImgCache.getCachedFileURL(src,
+              (originalUrl, cacheUrl) => {
+                resolve(cacheUrl);
+              },
+              (e) => {
+                reject(e)
+              });
+          } else {
+            // cache img
+            ImgCache.cacheFile(src);
+            // return original img URL
+            resolve(src);
+          }
+        });
+      }else{
+        resolve(src);
+      }
+    });
+  }
+}
